d639c7c3704d96d9a175e4d1f76f7811d9b7a4fa
nitish9t7 <40459163+nitish9t7@users.noreply.github.com>
2019-07-06 18:49:00 +0530

    Update priceTracker.py

--- priceTracker.py
+++ priceTracker.py
@@ -2,10 +2,10 @@ import time
 import requests
 from bs4 import BeautifulSoup
 import smtplib
-URL = input('Enter The Product URL')
+URL = input('Enter The Product URL')    //URL ot Product
 header = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
-to = input("To : ")
+to = input("To : ")                     //Email of Reciever
 def check():
     page = requests.get(URL, headers=header)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -22,18 +22,18 @@ def sendMail():
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login('nitish9t7@gmail.com', 'udidmvzbwrpaoumf')
+    server.login('*******@gmail.com', '**************')             //loginId and Apppassword of Sender
 
     subject = 'price fell down'
     body = URL
     msg = f"Subject: { subject}\n\n{body}"
     server.sendmail(
-        'nitish9t7@gmail.com',
-        to,
-        msg
+        '********@gmail.com',                           // Email of Sender
+        to,                                                // Emailof Reciever
+        msg                                                 // Message 
     )
     server.quit()
     print('sent')
-while(True):
+while(True):                                                   //checking at Specified interval of time
     check()
     time.sleep(3)
